a9fa79b0ec7a6cd862ddde3f98224a45b1c5e5f4
Quaternions <krakow20@gmail.com>
2025-02-24 13:44:42 -0800

    deserialize bool with DeserializeSeed

--- src/data/entity.rs
+++ src/data/entity.rs
@@ -359,12 +359,61 @@ impl<'de> Deserialize<'de> for Negated {
     }
 }
 
+struct BoolSeed;
+impl<'de> DeserializeSeed<'de> for BoolSeed {
+    type Value = bool;
+
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(BoolVisitor)
+    }
+}
+
+struct BoolVisitor;
+impl<'de> serde::de::Visitor<'de> for BoolVisitor {
+    type Value = bool;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "bool value")
+    }
+
+    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match v {
+            0 => Ok(false),
+            1 => Ok(true),
+            _ => Err(E::invalid_value(Unexpected::Unsigned(v), &"0 or 1")),
+        }
+    }
+
+    fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match v {
+            0 => Ok(false),
+            1 => Ok(true),
+            _ => Err(E::invalid_value(Unexpected::Signed(v), &"0 or 1")),
+        }
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match v {
+            "0" | "no" => Ok(false),
+            "1" | "yes" => Ok(true),
+            other => Err(E::invalid_value(Unexpected::Str(other), &"bool")),
+        }
+    }
+}
+
 #[allow(dead_code)]
 pub fn bool_from_int<'de, D: Deserializer<'de>>(deserializer: D) -> Result<bool, D::Error> {
-    let s = <&str>::deserialize(deserializer)?;
-    match s {
-        "0" | "no" => Ok(false),
-        "1" | "yes" => Ok(true),
-        other => Err(D::Error::invalid_value(Unexpected::Str(other), &"bool")),
-    }
+    BoolSeed.deserialize(deserializer)
 }
